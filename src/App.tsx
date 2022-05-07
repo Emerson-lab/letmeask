6b8bbc5e8e2acbd1d08e2931184e31a5e167241a
Emerson Trindade <trinddade29@gmail.com>
2022-05-06 23:02:26 -0300

    conection to firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
+
 function App() {
+  const [number, setNumber] = useState<number>(0);
+  
   return (
     <div>
-      <h2>Welcome!</h2>
+      <button
+        onClick={() => {
+          setNumber(number + 1)
+        }}
+      >
+        adcionar
+      </button>
+      <span>{number}</span>
+      <button
+        onClick={() => {
+          setNumber(number - 1)
+        }}
+      >
+        remover
+      </button>
     </div>
   );
 }
